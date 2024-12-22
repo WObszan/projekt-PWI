9af44184af5bc0074493fbd332d472fe3a99df2a
xmaciejson <maciej.brandys17@gmail.com>
2024-12-22 23:37:37 +0100

    Stworzenie funkcji sortującej oraz dodanie opisów

--- projekt-pwi/filtry-sortowanie.py
+++ projekt-pwi/filtry-sortowanie.py
@@ -1,7 +1,18 @@
 class FiltrySortowanie:
 
     def __init__(self, file_name):
+        '''
+        :param file_name: Nazwa pliku
+        '''
         with open(file_name, 'r') as file:
             self.tasks = file.readlines()
 
-    def sort_by_date(self, key):
+    def sort_tasks(self, key, reverse):
+        '''
+        :param key: Klucz, wedle którego będziemy sortować nasz plik
+        :param reverse: Wartość, która mówi czy sortujemy rosnąco lub malejąco
+        :return: Zwraca posortowany słownik
+        '''
+        return sorted(self.tasks, key=lambda task: task[key], reverse=reverse)
+
+
