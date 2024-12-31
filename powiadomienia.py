2b461038d96d6adbdce6ad360df08c23fb5c6967
wojciechobszanski <obszanskiwojciech@gmail.com>
2024-12-31 11:11:28 +0100

    testowa wersja na wysylanie emaili bardziej czytelna

--- powiadomienia.py
+++ powiadomienia.py
@@ -4,8 +4,11 @@ my_email = "t0.d0.l1st.pwi@gmail.com"
 app_password = "oidg goxj cgci nqrp"
 user_mail = "wo.playstation@gmail.com"
 message = "Test"
-connection = smtplib.SMTP('smtp.gmail.com', 587)
-connection.starttls()
-connection.login(user= my_email, password=app_password)
-connection.sendmail(from_addr=my_email,to_addrs=user_mail, msg=message)
-connection.close()
+with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+    connection.starttls()
+    connection.login(user= my_email, password=app_password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs=user_mail,
+        msg=f"Subject:Reminder\n\n {message}"
+    )
