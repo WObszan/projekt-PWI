65ce5b897cee6d3054556574790e75b9820aa118
wojciechobszanski <obszanskiwojciech@gmail.com>
2024-12-31 11:03:03 +0100

    testowa wersja na wysylanie emaili

---
+++ powiadomienia.py
@@ -0,0 +1,11 @@
+### Powiadomienia ###
+import smtplib
+my_email = "t0.d0.l1st.pwi@gmail.com"
+app_password = "oidg goxj cgci nqrp"
+user_mail = "wo.playstation@gmail.com"
+message = "Test"
+connection = smtplib.SMTP('smtp.gmail.com', 587)
+connection.starttls()
+connection.login(user= my_email, password=app_password)
+connection.sendmail(from_addr=my_email,to_addrs=user_mail, msg=message)
+connection.close()
