7cb0ac17cfb12a149a6341bf9fb3d03e7adfd2de
1234 <353535@uwr.edu.pl>
2025-01-14 23:42:14 +0100

    Stworzylem folder validacja plikow i w nim bedzie kod, ktory po otrzymaniu pliku json sprawdza czy plik jest zapisany w oczekiwanym formacie

---
+++ validacja plikow jason/validacja.py
@@ -0,0 +1,7 @@
+import json
+from datetime import datetime
+
+def validate_tasks(tasks):
+	#funkcja ktora sprawdza czy wszystkie rzeczy w pliku jason sa zapisane w oczekiwanym formacie, a jesli nie to gdzie jest źle
+
+print("DONE")
