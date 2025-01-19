4280e9a5bd8303f84f98cae5cc396b0270373b26
1234 <353535@uwr.edu.pl>
2025-01-19 16:44:06 +0100

    Dodalem do pliku validacja funkcje ktora sprawdza czy czas data i email sa podane w poprawnym formacie bez zadnych bledow

--- validacja plikow jason/validacja.py
+++ validacja plikow jason/validacja.py
@@ -9,6 +9,23 @@ class TaskValidator:
 	def validate_tasks(self,task):
 		#funkcja ktora sprawdza pojedyncze zadanie czy jest poprawne
 	def validate_email(self,email):
-		#Funkcja ktora sprawdza czy email jest poprawnie zapisany (np. czy wystepuje "@")
+		#Funkcja ktora sprawdza czy email jest poprawnie zapisany 
+		email_pattern = r"^[^@]+@[^@]+\.[^@]+$"
+        return re.match(email_pattern, email) is not None
+    def validate_date(self,date):
+        #funkcja sprawdza czy data jest w poprawnym formacie
+        try:
+            datetime.strptime(date_text, "%Y-%m-%d")
+            return True
+        except ValueError:
+            return False
+    def validate_time(self,time):
+        #funkcja sprawdza czy godzina jest w poprawnym formacie
+        try:
+            datetime.strptime(time_text, "%H:%M")
+            return True
+        except ValueError:
+            return False
+
 	
 		
