f79f7013b966ec8b5611d2706cc15d15ba68f719
radoSo <radosochacki@gmail.com>
2025-01-21 10:53:00 +0100

    changed window size from 800x600 to 1200x600

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class TaskManagerApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Task Management Application")
-        self.root.geometry("800x600")
+        self.root.geometry("1200x600")
         
         # Initialize necessary classes
         self.filtry_sortowanie = FiltrySortowanie(TASKS_FILE)
